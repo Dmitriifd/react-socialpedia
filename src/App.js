ef7baef414f29a5b1c7cb85eb0e8b3219911f061
Dmitriifd <idimka9@gmail.com>
2023-05-15 14:26:15 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/react-socialpedia">
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
